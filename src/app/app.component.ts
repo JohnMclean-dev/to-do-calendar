a2a7b3c24e0cc225659c4c70b4f1f5d28bc019b7
dijon64 <john1.mclean@outlook.com>
2022-08-06 18:26:19 -0400

    add click event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,20 @@ import { CalendarOptions } from '@fullcalendar/angular';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth'
+    initialView: 'dayGridMonth',
+    dateClick: this.handleDateClick.bind(this),
+    events: [
+      { title: 'event 1', date: '2022-08-17' },
+      { title: 'event 2', date: '2022-08-22' }
+    ]
   };
+
+  handleDateClick(arg: any) {
+    alert('date click!' + arg.dateStr)
+  }
+
 }
